726209128467075ff45d0e123e2aa991491a1f9c
Justin Steffy <steffy@orgsync.com>
2013-06-12 17:10:31 -0500

    initial project setup and a few classes

---
+++ src/main/java/com/orgsync/api/OrgsyncRestClient.java
@@ -0,0 +1,12 @@
+package com.orgsync.api;
+
+public class OrgsyncRestClient {
+	
+	private static final String HOST = "https://api.orgsync.com/api";
+	
+	
+	
+			
+
+
+}
---
+++ src/test/java/com/orgsync/api/OrgsyncRestClientTest.java
@@ -0,0 +1,14 @@
+package com.orgsync.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrgsyncRestClientTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
