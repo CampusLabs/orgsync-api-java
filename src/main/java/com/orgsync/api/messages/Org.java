db380813073272f2d9b39c9277ee0478b3a60888
Justin Steffy <steffy@orgsync.com>
2013-06-13 13:24:09 -0500

    generate hashcode and equals

--- src/main/java/com/orgsync/api/messages/Org.java
+++ src/main/java/com/orgsync/api/messages/Org.java
@@ -26,6 +26,49 @@ public class Org {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = prime * result + id;
+		result = prime * result
+				+ ((longName == null) ? 0 : longName.hashCode());
+		result = prime * result
+				+ ((shortName == null) ? 0 : shortName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Org other = (Org) obj;
+		if (createdAt == null) {
+			if (other.createdAt != null)
+				return false;
+		} else if (!createdAt.equals(other.createdAt))
+			return false;
+		if (id != other.id)
+			return false;
+		if (longName == null) {
+			if (other.longName != null)
+				return false;
+		} else if (!longName.equals(other.longName))
+			return false;
+		if (shortName == null) {
+			if (other.shortName != null)
+				return false;
+		} else if (!shortName.equals(other.shortName))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Org [id=" + id + ", shortName=" + shortName + ", longName="
 				+ longName + ", createdAt=" + createdAt + "]";
