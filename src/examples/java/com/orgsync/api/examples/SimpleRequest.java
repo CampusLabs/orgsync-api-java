3f26de308d8656d635e7be51e8d0e62033e1f5d0
Justin Steffy <steffy@orgsync.com>
2013-06-14 11:13:52 -0500

    Fix the poor example client which I badly broke...  Now exits cleanly.

--- src/examples/java/com/orgsync/api/examples/SimpleRequest.java
+++ src/examples/java/com/orgsync/api/examples/SimpleRequest.java
@@ -16,23 +16,24 @@ public class SimpleRequest {
 	public static void main(final String[] args) throws InterruptedException,
 			ExecutionException {
 		String apiKey = "dd6b9d2beb614611c5eb9f56c34b743d1d86f385";
-		String host = "http://localhost/api";
+		String host = "http://localhost:8080/api/";
 		ApiClient client = OrgSync.newApiClient(apiKey, Version.V2, host);
 
-		System.out.println("Requesting orgs");
-		OrgsModule module = client.getModule(Modules.ORGS);
-		ApiResponse<List<Org>> result = module.getOrgs().get();
-
-		if (result.isSuccess()) {
-			System.out.println("Recieved following orgs:");
-			System.out.println(joinList(result.getResult(), "\n"));
-		} else {
-			System.err.println("Error attempting to retrieve orgs!");
-			System.err.println(result.getError());
+		try {
+			System.out.println("Requesting orgs");
+			OrgsModule module = client.getModule(Modules.ORGS);
+			ApiResponse<List<Org>> result = module.getOrgs().get();
+			if (result.isSuccess()) {
+				System.out.println("Recieved following orgs:");
+				System.out.println(joinList(result.getResult(), "\n"));
+			} else {
+				System.err.println("Error attempting to retrieve orgs!");
+				System.err.println(result.getError());
+			}
+			System.out.println("Cleanup client");
+		} finally {
+			client.destroy();
 		}
-
-		System.out.println("Cleanup client");
-		client.destroy();
 
 		System.out.println("Exiting...");
 	}
