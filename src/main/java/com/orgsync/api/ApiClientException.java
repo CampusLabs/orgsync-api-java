b5057bf242d94482b82d84262d238ef0089ee18d
Justin Steffy <steffy@orgsync.com>
2013-06-19 11:01:16 -0500

    Hide ApiClientException constructor and add some javadoc.

--- src/main/java/com/orgsync/api/ApiClientException.java
+++ src/main/java/com/orgsync/api/ApiClientException.java
@@ -1,9 +1,16 @@
 package com.orgsync.api;
 
+/**
+ * An exception in the {@link ApiClient} that has caused a failure to retrieve the requested information. The
+ * {@link #getCause()} will return the exception that caused this failure.
+ * 
+ * @author steffyj
+ * 
+ */
 @SuppressWarnings("serial")
 public class ApiClientException extends RuntimeException {
 
-    public ApiClientException(final String message, final Exception e) {
+    /* package */ApiClientException(final String message, final Exception e) {
         super(message, e);
     }
 }
