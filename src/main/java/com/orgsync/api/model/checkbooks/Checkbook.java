d099bf2df2014ce51b1e1f50eba50d5cc5c96f37
Justin Steffy <steffy@orgsync.com>
2013-06-19 15:21:24 -0500

    Models for the checkbook entry stuff.

---
+++ src/main/java/com/orgsync/api/model/checkbooks/Checkbook.java
@@ -0,0 +1,82 @@
+package com.orgsync.api.model.checkbooks;
+
+import com.orgsync.api.model.IdAndName;
+
+public class Checkbook {
+
+    private int id;
+    private String name;
+    private boolean isReadOnly;
+    private String balance;
+    private IdAndName org;
+
+    public final int getId() {
+        return id;
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    public final boolean isReadOnly() {
+        return isReadOnly;
+    }
+
+    public final String getBalance() {
+        return balance;
+    }
+
+    public final IdAndName getOrg() {
+        return org;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((balance == null) ? 0 : balance.hashCode());
+        result = prime * result + id;
+        result = prime * result + (isReadOnly ? 1231 : 1237);
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((org == null) ? 0 : org.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Checkbook other = (Checkbook) obj;
+        if (balance == null) {
+            if (other.balance != null)
+                return false;
+        } else if (!balance.equals(other.balance))
+            return false;
+        if (id != other.id)
+            return false;
+        if (isReadOnly != other.isReadOnly)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (org == null) {
+            if (other.org != null)
+                return false;
+        } else if (!org.equals(other.org))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Checkbook [id=" + id + ", name=" + name + ", isReadOnly=" + isReadOnly + ", balance=" + balance
+                + ", org=" + org + "]";
+    }
+
+}
---
+++ src/main/java/com/orgsync/api/model/checkbooks/CheckbookEntry.java
@@ -0,0 +1,95 @@
+package com.orgsync.api.model.checkbooks;
+
+import java.util.Date;
+
+import com.orgsync.api.model.IdAndName;
+
+public class CheckbookEntry {
+
+    private int id;
+    private String description;
+    private boolean isReadOnly;
+    private String amount;
+    private Date occurredAt;
+    private IdAndName checkbook;
+
+    public final int getId() {
+        return id;
+    }
+
+    public final String getDescription() {
+        return description;
+    }
+
+    public final boolean isReadOnly() {
+        return isReadOnly;
+    }
+
+    public final String getAmount() {
+        return amount;
+    }
+
+    public final Date getOccurredAt() {
+        return occurredAt;
+    }
+
+    public final IdAndName getCheckbook() {
+        return checkbook;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+        result = prime * result + ((checkbook == null) ? 0 : checkbook.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + id;
+        result = prime * result + (isReadOnly ? 1231 : 1237);
+        result = prime * result + ((occurredAt == null) ? 0 : occurredAt.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CheckbookEntry other = (CheckbookEntry) obj;
+        if (amount == null) {
+            if (other.amount != null)
+                return false;
+        } else if (!amount.equals(other.amount))
+            return false;
+        if (checkbook == null) {
+            if (other.checkbook != null)
+                return false;
+        } else if (!checkbook.equals(other.checkbook))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (id != other.id)
+            return false;
+        if (isReadOnly != other.isReadOnly)
+            return false;
+        if (occurredAt == null) {
+            if (other.occurredAt != null)
+                return false;
+        } else if (!occurredAt.equals(other.occurredAt))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckbookEntry [id=" + id + ", description=" + description + ", isReadOnly=" + isReadOnly + ", amount="
+                + amount + ", occurredAt=" + occurredAt + ", checkbook=" + checkbook + "]";
+    }
+
+}
