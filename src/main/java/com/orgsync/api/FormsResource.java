679f787da4dd145f2836387f0a53ae071013d7e2
Justin Steffy <steffy@orgsync.com>
2013-06-19 15:57:46 -0500

    First crack at getFormSubmissions

--- src/main/java/com/orgsync/api/FormsResource.java
+++ src/main/java/com/orgsync/api/FormsResource.java
@@ -1,5 +1,10 @@
 package com.orgsync.api;
 
+import com.ning.http.client.ListenableFuture;
+import com.orgsync.api.model.forms.FormSubmission;
+
 public interface FormsResource {
+
+    public ListenableFuture<ApiResponse<FormSubmission>> getFormSubmission(int submissionId);
 
 }
--- src/main/java/com/orgsync/api/FormsResourceImpl.java
+++ src/main/java/com/orgsync/api/FormsResourceImpl.java
@@ -1,9 +1,21 @@
 package com.orgsync.api;
 
+import com.google.gson.reflect.TypeToken;
+import com.ning.http.client.ListenableFuture;
+import com.orgsync.api.model.forms.FormSubmission;
+
 /*package*/class FormsResourceImpl extends BaseResource implements FormsResource {
 
     /* package */FormsResourceImpl(final ApiClientImpl client) {
         super(client);
+    }
+
+    @Override
+    public ListenableFuture<ApiResponse<FormSubmission>> getFormSubmission(final int submissionId) {
+        return getResponse(
+                RequestParams.get("/form_submissions/" + submissionId),
+                new TypeToken<FormSubmission>() {
+                }.getType());
     }
 
 }
