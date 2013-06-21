6cbf1d07ac7a1ddb379c2ed9f0e248d4e400c4ed
Justin Steffy <steffy@orgsync.com>
2013-06-21 14:42:55 -0500

    Add integration tests and generating of db_template json in the gradle build file.

---
+++ src/integration/java/com/orgsync/api/integration/AccountsIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.orgsync.api.integration;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AccountsIntegrationTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
---
+++ src/integration/java/com/orgsync/api/integration/DbTemplate.java
@@ -0,0 +1,35 @@
+package com.orgsync.api.integration;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigRenderOptions;
+
+public class DbTemplate {
+
+	public static final Config CONFIG = ConfigFactory.load("db_template.conf")
+			.resolve();
+
+	public static void main(final String[] args) throws IOException {
+		File file = new File("build/db_template.json");
+		System.out.println("Generating template json to file: "
+				+ file.getAbsolutePath());
+
+		ConfigRenderOptions renderOptions = ConfigRenderOptions.defaults()
+				.setJson(true).setComments(false).setOriginComments(false);
+
+		String json = CONFIG.getValue("template").render(renderOptions);
+
+		FileWriter fileWriter = new FileWriter(file);
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+		bufferedWriter.write(json);
+
+		bufferedWriter.close();
+	}
+
+}
