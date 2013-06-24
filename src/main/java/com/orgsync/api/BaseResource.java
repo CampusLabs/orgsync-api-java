294e7bedc54a23abbb62bf497092ccc4d027ff3a
Justin Steffy <steffy@orgsync.com>
2013-06-24 16:32:04 -0500

    add more documentation.

--- src/main/java/com/orgsync/api/BaseResource.java
+++ src/main/java/com/orgsync/api/BaseResource.java
@@ -8,11 +8,28 @@ import java.util.TimeZone;
 import com.ning.http.client.FluentStringsMap;
 import com.ning.http.client.ListenableFuture;
 
-/*package*/abstract class BaseResource {
+/**
+ * <p>
+ * An Abstract Base Class (ABC) for resources. It provides some commonly needed functions to map common rails idioms.
+ * For instance, {@link #list(Type)} will return the standard index for a given endpoint and create the correct type.
+ * <p>
+ * Some other helpers include {@link #dateToQueryParam(Date)} for converting a date to the correct format. Also the
+ * {@link #checkAddField(FluentStringsMap, String, Object)} will add a given field to the query params with a given name
+ * if it is not null. This is useful for the various <code>*UpdateRequest</code> methods.
+ * 
+ * 
+ * @author steffyj
+ * 
+ */
+/* package */abstract class BaseResource {
 
-    /* package */static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    /**
+     * The date format to use (e.g. in query params) /* package
+     */
+    static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     static {
+        // Don't forget to set the timezone...
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 
@@ -20,51 +37,142 @@ import com.ning.http.client.ListenableFuture;
 
     private final String endpoint;
 
+    /**
+     * Every resource needs an {@link ApiClientImpl} to make requests the and the base endpoint.
+     * 
+     * @param client
+     *            the client to use for making requests
+     * @param endpoint
+     *            the base endpoint for this resource
+     */
     /* package */BaseResource(final ApiClientImpl client, final String endpoint) {
         this.client = client;
         this.endpoint = endpoint;
     }
 
+    /**
+     * Get a response for the request params and type... delegate to client.
+     * 
+     * @param params
+     *            the request info
+     * @param type
+     *            the type that is returned on success
+     * @return the future of the response for the request
+     */
     /* package */<T> ListenableFuture<ApiResponse<T>> getResponse(final RequestParams params, final Type type) {
         return client.getResponse(params, type);
     }
 
-    /* package */String dateToQueryParam(final Date date) {
-        return dateFormat.format(date);
-    }
-
-    String getEndpoint() {
-        return endpoint;
-    }
-
-    String showFor(final int id) {
-        return String.format("%s/%d", endpoint, id);
-    }
-
-    <T> ListenableFuture<ApiResponse<T>> list(final Type type) {
-        return list("", type);
-    }
-
-    <T> ListenableFuture<ApiResponse<T>> list(final String prefix, final Type type) {
-        return getResponse(RequestParams.get(prefix + endpoint), type);
-    }
-
-    <T> ListenableFuture<ApiResponse<T>> show(final int id, final Type type) {
-        return getResponse(RequestParams.get(showFor(id)), type);
-    }
-
-    <T> ListenableFuture<ApiResponse<T>> update(final int id, final FluentStringsMap params, final Type type) {
-        return getResponse(RequestParams.put(showFor(id), params), type);
-    }
-
-    <T> ListenableFuture<ApiResponse<T>> delete(final int id, final Type type) {
-        return getResponse(RequestParams.delete(showFor(id)), type);
-    }
-
+    /**
+     * Add a value to the params with the field name if it is not <code>null</code>.
+     * 
+     * @param params
+     *            the params to add to
+     * @param field
+     *            the field name in the params
+     * @param value
+     *            the value to add if not null
+     */
     void checkAddField(final FluentStringsMap params, final String field, final Object value) {
         if (value != null) {
             params.add(field, value.toString());
         }
+    }
+
+    /**
+     * Convert a date to a query param string.
+     * 
+     * @param date
+     *            the date to convert
+     * @return the string for the query param
+     */
+    /* package */String dateToQueryParam(final Date date) {
+        return dateFormat.format(date);
+    }
+
+    /**
+     * Get the endpoint.
+     * 
+     * @return the endpoint
+     */
+    String getEndpoint() {
+        return endpoint;
+    }
+
+    /**
+     * Get the show URL for a given ID.
+     * 
+     * @param id
+     *            the id to get the show URL for
+     * @return the show URL
+     */
+    String showFor(final int id) {
+        return String.format("%s/%d", endpoint, id);
+    }
+
+    /**
+     * List this resource by calling the endpoint and use the given type.
+     * 
+     * @param type
+     *            the return type of the list
+     * @return the future to the response
+     */
+    <T> ListenableFuture<ApiResponse<T>> list(final Type type) {
+        return list("", type);
+    }
+
+    /**
+     * Call to list with some given prefix. This is a common idiom in the API (e.g. /orgs/123/accounts).
+     * 
+     * @param prefix
+     *            the prefix to use
+     * @param type
+     *            the type that is returned
+     * @return the future to the response
+     */
+    <T> ListenableFuture<ApiResponse<T>> list(final String prefix, final Type type) {
+        return getResponse(RequestParams.get(prefix + endpoint), type);
+    }
+
+    /**
+     * A standard show for a given id.
+     * 
+     * @param id
+     *            the id to show
+     * @param type
+     *            the type of result
+     * @return the future to the response
+     */
+    <T> ListenableFuture<ApiResponse<T>> show(final int id, final Type type) {
+        return getResponse(RequestParams.get(showFor(id)), type);
+    }
+
+    /**
+     * Update a given id using the given params.
+     * 
+     * @param id
+     *            the id to update
+     * @param params
+     *            the query params to use in the update
+     * @param type
+     *            the type that is returned
+     * @return the future to the response
+     */
+    <T> ListenableFuture<ApiResponse<T>> update(final int id, final FluentStringsMap params, final Type type) {
+        return getResponse(RequestParams.put(showFor(id), params), type);
+    }
+
+    /**
+     * Delete the given id.
+     * 
+     * @param id
+     *            the id to delete
+     * @param type
+     *            the response type
+     * @return the future to the response
+     */
+    <T> ListenableFuture<ApiResponse<T>> delete(final int id, final Type type) {
+        return getResponse(RequestParams.delete(showFor(id)), type);
     }
 
 }
--- src/main/java/com/orgsync/api/CheckbooksResource.java
+++ src/main/java/com/orgsync/api/CheckbooksResource.java
@@ -7,29 +7,135 @@ import com.orgsync.api.model.Success;
 import com.orgsync.api.model.checkbooks.Checkbook;
 import com.orgsync.api.model.checkbooks.CheckbookEntry;
 
+/**
+ * <p>
+ * Access to the checkbooks resource from the OrgSync API. This allows a client to get all checkbooks, create, update
+ * and delete checkbooks. A client can also manage entries for a checkbook.
+ * <p>
+ * See: <a href="https://api.orgsync.com/api/docs/v2/checkbooks">https://api.orgsync.com/api/docs/v2/checkbooks</a>
+ * 
+ * @author steffyj
+ * 
+ */
 public interface CheckbooksResource {
 
-    public ListenableFuture<ApiResponse<List<Checkbook>>> getCheckbooks();
+    /**
+     * Get all of the checkbooks.
+     * 
+     * @return a future to the response of all checkbooks
+     */
+    public ListenableFuture<ApiResponse<List<Checkbook>>>
+            getCheckbooks();
 
-    public ListenableFuture<ApiResponse<Checkbook>> getCheckbook(int checkbookId);
+    /**
+     * Get a single checkbook.
+     * 
+     * @param checkbookId
+     *            the id of the checkbook to get
+     * @return a future to the response of the checkbook
+     */
+    public ListenableFuture<ApiResponse<Checkbook>>
+            getCheckbook(int checkbookId);
 
-    public ListenableFuture<ApiResponse<Checkbook>> createCheckbook(int orgId, String name);
+    /**
+     * Create a new checkbook for a given or with a given name.
+     * 
+     * @param orgId
+     *            the org that owns this checkbook
+     * @param name
+     *            the name of the checkbook
+     * @return a future to the response of the new checkbook
+     */
+    public ListenableFuture<ApiResponse<Checkbook>>
+            createCheckbook(int orgId, String name);
 
-    public ListenableFuture<ApiResponse<Checkbook>> updateCheckbook(int checkbookId, String name);
+    /**
+     * Update the name of a given checkbook.
+     * 
+     * @param checkbookId
+     *            the id of the checkbook
+     * @param name
+     *            the updated name
+     * @return a future to the response of the checkbook
+     */
+    public ListenableFuture<ApiResponse<Checkbook>>
+            updateCheckbook(int checkbookId, String name);
 
-    public ListenableFuture<ApiResponse<Success>> deleteCheckbook(int checkbookId);
+    /**
+     * Delete the checkbook with the given id.
+     * 
+     * @param checkbookId
+     *            the id of the checkbook to delete
+     * @return a future to the response
+     */
+    public ListenableFuture<ApiResponse<Success>>
+            deleteCheckbook(int checkbookId);
 
-    public ListenableFuture<ApiResponse<List<Checkbook>>> getOrgCheckbooks(int orgId);
+    /**
+     * Get the checkbooks for a given org.
+     * 
+     * @param orgId
+     *            the org to get checkbooks for
+     * @return a future to the response to a list of checkbooks
+     */
+    public ListenableFuture<ApiResponse<List<Checkbook>>>
+            getOrgCheckbooks(int orgId);
 
-    public ListenableFuture<ApiResponse<List<CheckbookEntry>>> getCheckbookEntries(int checkbookId);
+    /**
+     * Get the checkbook entries for a given checkbook.
+     * 
+     * @param checkbookId
+     *            the id of the checkbook
+     * @return a future to the response of the checkbook entries
+     */
+    public ListenableFuture<ApiResponse<List<CheckbookEntry>>>
+            getCheckbookEntries(int checkbookId);
 
-    public ListenableFuture<ApiResponse<CheckbookEntry>> getCheckbookEntry(int checkbookEntryId);
+    /**
+     * Get a specific checkbook entry
+     * 
+     * @param checkbookEntryId
+     *            the id of the checkbook entry
+     * @return a future to the response of the checkbook entry
+     */
+    public ListenableFuture<ApiResponse<CheckbookEntry>>
+            getCheckbookEntry(int checkbookEntryId);
 
+    /**
+     * Create a checkbook entry in the given checkbook with the amount and description.
+     * 
+     * @param checkbookId
+     *            the id of the checkbook
+     * @param amount
+     *            the amount of the entry
+     * @param description
+     *            the description for this entry
+     * @return a future to the response of the new checkbook entry
+     */
     public ListenableFuture<ApiResponse<CheckbookEntry>>
             createCheckbookEntry(int checkbookId, String amount, String description);
 
+    /**
+     * Update a checkbook entry's amount and description
+     * 
+     * @param checkbookEntryId
+     *            the id of the entry to update
+     * @param amount
+     *            the updated amount
+     * @param description
+     *            the updated description
+     * @return the future to the response of the updated checkbook entry
+     */
     public ListenableFuture<ApiResponse<CheckbookEntry>>
             updateCheckbookEntry(int checkbookEntryId, String amount, String description);
 
-    public ListenableFuture<ApiResponse<Success>> deleteCheckbookEntry(int checkbookEntryId);
+    /**
+     * Delete a checkbook entry.
+     * 
+     * @param checkbookEntryId
+     *            the id of the entry to delete
+     * @return a future to the response
+     */
+    public ListenableFuture<ApiResponse<Success>>
+            deleteCheckbookEntry(int checkbookEntryId);
 }
