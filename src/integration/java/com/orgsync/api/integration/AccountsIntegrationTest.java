457273bdc1319241d987ceb7af90df3748013cb4
Justin Steffy <steffy@orgsync.com>
2013-07-25 11:14:08 -0500

    Add a bunch of tests around the custom profile, account search and updating accounts.

--- src/integration/java/com/orgsync/api/integration/AccountsIntegrationTest.java
+++ src/integration/java/com/orgsync/api/integration/AccountsIntegrationTest.java
@@ -1,11 +1,13 @@
 package com.orgsync.api.integration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hamcrest.core.IsCollectionContaining;
 import org.junit.AfterClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.orgsync.api.AccountsResource;
@@ -13,6 +15,9 @@ import com.orgsync.api.Resources;
 import com.orgsync.api.model.accounts.Account;
 import com.orgsync.api.model.accounts.AccountDetail;
 import com.orgsync.api.model.accounts.AccountFull;
+import com.orgsync.api.model.accounts.AccountUpdateRequest;
+import com.orgsync.api.model.accounts.AccountUpdateRequest.ElementPair;
+import com.orgsync.api.model.accounts.CustomProfileField;
 import com.typesafe.config.Config;
 
 public class AccountsIntegrationTest extends BaseIntegrationTest<AccountsResource> {
@@ -57,21 +62,61 @@ public class AccountsIntegrationTest extends BaseIntegrationTest<AccountsResourc
     }
 
     @Test
-    @Ignore("TODO")
     public void testGetAccountByCustomProfile() throws Exception {
-        // TODO
+        String profileField = accountConfig.getConfigList("profile").get(0).getString("data");
+        AccountDetail result = getResult(getResource().getAccountByCustomProfile(profileField));
+
+        assertEquals(accountConfig.getString("username"), result.getUsername());
     }
 
     @Test
-    @Ignore("TODO")
     public void testGetCustomProfileFields() throws Exception {
-        // TODO
+        List<? extends Config> profileFieldsConfig = DbTemplate.getList("custom_profile");
+        List<CustomProfileField> result = getResult(getResource().getCustomProfileFields());
+
+        List<String> configNames = new ArrayList<String>();
+        for (Config fieldConfig : profileFieldsConfig) {
+            configNames.add(fieldConfig.getString("name"));
+        }
+
+        List<String> resultNames = new ArrayList<String>();
+        for (CustomProfileField field : result) {
+            resultNames.add(field.getName());
+        }
+
+        assertThat(resultNames, IsCollectionContaining.hasItems(configNames.toArray(new String[configNames.size()])));
     }
 
     @Test
-    @Ignore("TODO")
     public void testUpdateAccount() throws Exception {
-        // TODO
-    }
+        Config accountConfig = configAccounts.get(1);
+        int accountId = accountConfig.getInt("id");
+        Config accountProfile = accountConfig.getConfigList("profile").get(0);
 
+        Config profileConfig = DbTemplate.getList("custom_profile").get(0);
+
+        String updatedLastName = "j";
+        String profileUpdate = "updated";
+
+        AccountFull original = getResult(getResource().getAccount(accountId));
+
+        assertEquals(accountConfig.getString("last_name"), original.getLastName());
+        assertEquals(accountProfile.getString("data"), original.getProfileResponses().get(0).getData());
+
+        AccountFull result = getResult(getResource().updateAccount(accountId,
+                new AccountUpdateRequest()
+                        .setLastName(updatedLastName)
+                        .setElement(new ElementPair(profileConfig.getInt("id"), profileUpdate))));
+
+        assertEquals(updatedLastName, result.getLastName());
+        assertEquals(profileUpdate, result.getProfileResponses().get(0).getData());
+
+        AccountFull reverted = getResult(getResource().updateAccount(accountId,
+                new AccountUpdateRequest()
+                        .setLastName(accountConfig.getString("last_name"))
+                        .setElement(new ElementPair(profileConfig.getInt("id"), accountProfile.getString("data")))));
+
+        assertEquals(accountConfig.getString("last_name"), reverted.getLastName());
+        assertEquals(accountProfile.getString("data"), reverted.getProfileResponses().get(0).getData());
+    }
 }
