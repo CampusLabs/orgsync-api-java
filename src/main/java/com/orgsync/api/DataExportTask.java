39d62ee0c38609db0ccb05f8e2c945cca97a74b6
Justin Steffy <steffy@orgsync.com>
2014-07-24 14:40:09 -0500

    Add some more base implementation layout.
    + Create the `executor` in the 'ExportsResourceImpl'
    + Add the `DataExportTask` for submitting to the executor
    + Add the `requestToken` call for getting a request

---
+++ src/main/java/com/orgsync/api/DataExportTask.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014 OrgSync
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package com.orgsync.api;
+
+import com.orgsync.api.model.ApiError;
+import com.orgsync.api.model.accounts.AccountFull;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+/**
+ * A task that handles the full life-cycle of getting an export.
+ *
+ * @author steffyj
+ */
+/* package */class DataExportTask<T> implements Callable<ApiResponse<T>> {
+
+    private final ExportsResourceImpl exports;
+    private final String exportType;
+
+    public DataExportTask(ExportsResourceImpl exports, String exportType) {
+        this.exports = exports;
+        this.exportType = exportType;
+    }
+
+    @Override
+    public ApiResponse<T> call() throws Exception {
+        ApiResponse<ExportsResourceImpl.ExportRequest> response = exports.requestToken(exportType).get();
+
+        if (response.isSuccess()) {
+            return ApiResponseFactory.error(new ApiError(response.getResult().getExportToken()));
+        } else {
+            return ApiResponseFactory.error(response.getError());
+        }
+    }
+
+}
--- src/main/java/com/orgsync/api/ExportsResourceImpl.java
+++ src/main/java/com/orgsync/api/ExportsResourceImpl.java
@@ -15,9 +15,13 @@
 */
 package com.orgsync.api;
 
+import com.google.gson.reflect.TypeToken;
 import com.orgsync.api.model.accounts.AccountFull;
 
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
@@ -27,13 +31,31 @@ import java.util.concurrent.Future;
  */
 /* package */class ExportsResourceImpl extends BaseResource implements ExportsResource {
 
+    private static final ExecutorService executor = Executors.newCachedThreadPool(new NamedThreadFactory("api-exports"));
+
     /* package */ExportsResourceImpl(final ApiClientImpl client) {
         super(client, "/exports");
     }
 
     @Override
     public Future<ApiResponse<List<AccountFull>>> getAccounts() {
-        return null;
+        return executor.submit(new DataExportTask<List<AccountFull>>(this, "accounts"));
     }
 
+    /* package */Future<ApiResponse<ExportRequest>> requestToken(String exportType) {
+        String url = String.format("%s/%s", getEndpoint(), exportType);
+        return getResponse(RequestParams.get(url), ExportRequest.TYPE);
+    }
+
+    /* package */static class ExportRequest {
+
+        public static final Type TYPE = new TypeToken<ExportRequest>() {
+        }.getType();
+
+        private String exportToken;
+
+        public String getExportToken() {
+            return exportToken;
+        }
+    }
 }
